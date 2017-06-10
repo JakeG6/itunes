9566aa7b45322f9cb06c8e2a52746b301dc95e5d
Jake Guss <jake@Jakes-MacBook-Air.local>
2017-06-10 11:08:19 -0600

    project complete

--- js/itunesService.js
+++ js/itunesService.js
@@ -4,17 +4,49 @@ angular.module('itunes').service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+    // function getArtist(artist) {
+    //   this.$http({
+    //     method: 'JSONP',
+    //     url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK',
+    //   }).then;
+    // }
+
+
+    this.getSongData = function (artist) {
+      return $http({
+        method: 'JSONP',
+        url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK',
+      }).then(function(response){
+        console.log(11111, response);
+        var iTunesArray = [];
+        for (var i = 0; i < response.data.results.length; i++) {
+          var newObj = {}
+
+          newObj.Artist = response.data.results[i].artistName;
+          newObj.Song = response.data.results[i].trackName;
+          newObj.Collection = response.data.results[i].collectionName;
+          newObj.AlbumArt = response.data.results[i].artworkUrl100;
+          newObj.Type = response.data.results[i].kind;
+          newObj.CollectionPrice = response.data.results[i].CollectionPrice;
+          console.log(response.data.results[i]);
+
+          iTunesArray.push(newObj);
+
+        }
+        return iTunesArray;
+      });
+    }
+
 
 
 
 
     // Go to the next step in the README (Tie in your controller). You will come back to these instructions shortly.
-    // 
+    //
     // You need to sort the data you get back from the itunes API to be an object in the following format.
     /*
       AlbumArt: "http://a3.mzstatic.com/us/r30/Features4/v4/22/be/30/22be305b-d988-4525-453c-7203af1dc5a3/dj.srlprmuo.100x100-75.jpg"
@@ -25,8 +57,14 @@ angular.module('itunes').service('itunesService', function($http, $q){
       Play: "http://a423.phobos.apple.com/us/r1000/013/Music4/v4/4a/ab/7c/4aab7ce2-9a72-aa07-ac6b-2011b86b0042/mzaf_6553745548541009508.plus.aac.p.m4a"
       Type: "song"
   */
-  //the iTunes API is going to give you a lot more details than ng-grid wants. Create a new array and then loop through the iTunes data pushing into your new array objects that look like the above data. Make sure your method returns this finalized array of data. 
+
+  //the iTunes API is going to give you a lot more details than ng-grid wants.
+  //Create a new array and then loop through the iTunes data pushing into your
+  //new array objects that look like the above data.
+  //Make sure your method returns this finalized array of data.
   // When this is complete, head back to your controller.
+
+
 
 
 });
